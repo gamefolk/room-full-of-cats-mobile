56f5caaa80e78f3d00821bacc4f01b030f1123cc
Andy Russell <arussell123@gmail.com>
2015-07-02 18:51:26 -0500

    add iOS advertising support

---
+++ app/src/ios/java/org/gamefolk/roomfullofcats/IOSAdvertisingProvider.java
@@ -0,0 +1,28 @@
+package org.gamefolk.roomfullofcats;
+
+import org.robovm.apple.adsupport.ASIdentifierManager;
+import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.uikit.UIWebView;
+
+public class IOSAdvertisingProvider implements AdvertisingProvider {
+
+    @Override
+    public void initializeAdService() {
+        // No need for initialization
+    }
+
+    @Override
+    public String getAdvertisingIdentifier() {
+        return ASIdentifierManager.getSharedManager().getAdvertisingIdentifier().asString();
+    }
+
+    @Override
+    public String getUserAgent() {
+        UIWebView webView = new UIWebView(CGRect.Zero());
+        return webView.evaluateJavaScript("navigator.userAgent");
+    }
+
+    public boolean getDoNotTrack() {
+        return ASIdentifierManager.getSharedManager().isAdvertisingTrackingEnabled();
+    }
+}
